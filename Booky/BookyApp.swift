9babd48c79713d1c7bf683fc4ded8deb00789f76
Evangelos Spyromilios <71439594+V-Spyromilios@users.noreply.github.com>
2022-09-13 11:53:09 +0200

    Initial Commit

---
+++ Booky/BookyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookyApp.swift
+//  Booky
+//
+//  Created by Evangelos Spyromilios on 13.09.22.
+//
+
+import SwiftUI
+
+@main
+struct BookyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
